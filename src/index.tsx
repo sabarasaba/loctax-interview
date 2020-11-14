7f790d72fbfb2eccc316ede955617fbd2d1857ac
Ignacio Rivas <rivasign@gmail.com>
2020-11-14 11:26:18 +0100

    chore: remove deprecated graphql packages and use @apollo/client instead

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 import 'src/assets/css/main.css';
 import Layout from 'src/components/Layout';
 import SquadBuilder from 'src/features/squad-builder';
 
 const client = new ApolloClient({
-  uri: process.env.REACT_APP_POKE_ENDPOINT
+  uri: process.env.REACT_APP_POKE_ENDPOINT,
+  cache: new InMemoryCache()
 });
 
 ReactDOM.render(
